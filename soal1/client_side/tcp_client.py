4c09391a8d85c94476a42ebd438bfe00bdc42c65
rikiachmad12 <rikiachmad@yahoo.com>
2022-04-01 13:49:50 +0000

    add:ETS progjar

---
+++ soal1/client_side/tcp_client.py
@@ -0,0 +1,117 @@
+import sys
+import socket
+import json
+import logging
+import xmltodict
+import ssl
+import os
+import time
+import datetime
+import threading
+import random
+
+server_address = ('172.16.16.101', 12000)
+
+def make_socket(destination_address='localhost',port=12000):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (destination_address, port)
+       # logging.warning(f"connecting to {server_address}")
+        sock.connect(server_address)
+        return sock
+    except Exception as ee:
+        logging.warning(f"error {str(ee)}")
+
+def make_secure_socket(destination_address='localhost',port=10000):
+    try:
+        #get it from https://curl.se/docs/caextract.html
+
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.verify_mode=ssl.CERT_OPTIONAL
+        context.load_verify_locations(os.getcwd() + '/domain.crt')
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (destination_address, port)
+      #  logging.warning(f"connecting to {server_address}")
+        sock.connect(server_address)
+        secure_socket = context.wrap_socket(sock,server_hostname=destination_address)
+        logging.warning(secure_socket.getpeercert())
+        return secure_socket
+    except Exception as ee:
+        logging.warning(f"error {str(ee)}")
+
+def deserialisasi(s):
+    logging.warning(f"deserialisasi {s.strip()}")
+    return json.loads(s)
+    
+
+def send_command(command_str,is_secure=False):
+    alamat_server = server_address[0]
+    port_server = server_address[1]
+    if is_secure == True:
+        sock = make_secure_socket(alamat_server,port_server)
+    else:
+        sock = make_socket(alamat_server,port_server)
+
+   # logging.warning(f"connecting to {server_address}")
+    try:
+       # logging.warning(f"sending message ")
+        sock.sendall(command_str.encode())
+        # Look for the response, waiting until socket is done (no more data)
+        data_received="" #empty string
+        while True:
+            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
+            data = sock.recv(16)
+            if data:
+                #data is not empty, concat with previous content
+                data_received += data.decode()
+                if "\r\n\r\n" in data_received:
+                    break
+            else:
+                # no more data, stop the process by break
+                break
+        hasil = deserialisasi(data_received)
+        #logging.warning("data received from server:")
+        print(hasil)
+        return 
+    except Exception as ee:
+        logging.warning(f"error during data receiving {str(ee)}")
+        return False
+
+
+
+def getdatapemain(nomor=0,is_secure=False):
+    cmd=f"getdatapemain {nomor}\r\n\r\n"
+    hasil = send_command(cmd,is_secure=is_secure)
+    return hasil
+
+def lihatversi(is_secure=False):
+    cmd=f"versi \r\n\r\n"
+    hasil = send_command(cmd,is_secure=is_secure)
+    return hasil
+
+## Function request with multithreading
+def getlistpemain(jumlah=0, is_secure=False):
+    texec = dict()
+    catat_awal = datetime.datetime.now()
+    print(f"mengambil {jumlah} data")
+    for i in range (1, jumlah+1):
+        r = random.randint(1, 20)
+        texec[i] = threading.Thread(target=getdatapemain, args=(r,False))
+        texec[i].start()
+        ##hasil = getdatapemain(i, is_secure=is_secure)
+    for i in range (1, jumlah+1):
+        texec[i].join()
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}\n")
+
+if __name__=='__main__':
+    ## ambil 1 data
+    getlistpemain(1)
+    ## ambil 5 data
+    getlistpemain(5)
+    ## ambil 10 data
+    getlistpemain(10)
+    ## ambil 20 data
+    getlistpemain(20)
---
+++ soal1/server_side/.ipynb_checkpoints/tcp_server-checkpoint.py
@@ -0,0 +1,131 @@
+import sys
+import socket
+import logging
+import json
+import dicttoxml
+import os
+import ssl
+
+alldata = dict()
+alldata['1']=dict(nomor=1, nama="dean henderson", posisi="kiper")
+alldata['2']=dict(nomor=2, nama="luke shaw", posisi="bek kiri")
+alldata['3']=dict(nomor=3, nama="aaron wan-bissaka", posisi="bek kanan")
+alldata['4']=dict(nomor=4, nama="victor lindelof", posisi="bek tengah kanan")
+alldata['5']=dict(nomor=5, nama="john hammond", posisi="kiper")
+alldata['6']=dict(nomor=6, nama="chuck norris", posisi="bek kiri")
+alldata['7']=dict(nomor=7, nama="james andrew", posisi="bek kanan")
+alldata['8']=dict(nomor=8, nama="adam hammington", posisi="bek tengah kanan")
+alldata['9']=dict(nomor=9, nama="blake harris", posisi="kiper")
+alldata['10']=dict(nomor=10, nama="harry nolan", posisi="bek kiri")
+alldata['11']=dict(nomor=11, nama="christopher corden", posisi="bek kanan")
+alldata['12']=dict(nomor=12, nama="russel brook", posisi="bek tengah kanan")
+alldata['13']=dict(nomor=13, nama="simon cowell", posisi="kiper")
+alldata['14']=dict(nomor=14, nama="taylor johnson", posisi="bek kiri")
+alldata['15']=dict(nomor=15, nama="thomas addison", posisi="bek kanan")
+alldata['16']=dict(nomor=16, nama="brandon bright", posisi="bek tengah kanan")
+alldata['17']=dict(nomor=17, nama="miles miller", posisi="kiper")
+alldata['18']=dict(nomor=18, nama="matthew moris", posisi="bek kiri")
+alldata['19']=dict(nomor=19, nama="chester bennington", posisi="bek kanan")
+alldata['20']=dict(nomor=20, nama="abraham lucas", posisi="bek tengah kanan")
+
+def versi():
+    return "versi 0.0.1"
+
+
+def proses_request(request_string):
+    #format request
+    # NAMACOMMAND spasi PARAMETER
+    cstring = request_string.split(" ")
+    hasil = None
+    try:
+        command = cstring[0].strip()
+        if (command == 'getdatapemain'):
+            # getdata spasi parameter1
+            # parameter1 harus berupa nomor pemain
+            logging.warning("getdata")
+            nomorpemain = cstring[1].strip()
+            try:
+                logging.warning(f"data {nomorpemain} ketemu")
+                hasil = alldata[nomorpemain]
+            except:
+                hasil = None
+        elif (command == 'versi'):
+            hasil = versi()
+    except:
+        hasil = None
+    return hasil
+
+
+def serialisasi(a):
+    serialized =  json.dumps(a)
+    # logging.warning("serialized data")
+    # logging.warning(serialized)
+    return serialized
+
+def run_server(server_address,is_secure=False):
+    # ------------------------------ SECURE SOCKET INITIALIZATION ----
+    if is_secure == True:
+        print(os.getcwd())
+        cert_location = os.getcwd() + '/certs/'
+        socket_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        socket_context.load_cert_chain(
+            certfile=cert_location + 'domain.crt',
+            keyfile=cert_location + 'domain.key'
+        )
+    # ---------------------------------
+
+    #--- INISIALISATION ---
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Bind the socket to the port
+    logging.warning(f"starting up on {server_address}")
+    sock.bind(server_address)
+    # Listen for incoming connections
+    sock.listen(1000)
+
+    while True:
+        # Wait for a connection
+        logging.warning("waiting for a connection")
+        koneksi, client_address = sock.accept()
+        logging.warning(f"Incoming connection from {client_address}")
+
+        try:
+            if is_secure == True:
+                connection = socket_context.wrap_socket(koneksi, server_side=True)
+            else:
+                connection = koneksi
+
+            selesai=False
+            data_received="" #string
+            while True:
+                data = connection.recv(32)
+                logging.warning(f"received {data}")
+                if data:
+                    data_received += data.decode()
+                    if "\r\n\r\n" in data_received:
+                        selesai=True
+
+                    if (selesai==True):
+                        hasil = proses_request(data_received)
+                        logging.warning(f"hasil proses: {hasil}\n")
+
+                        hasil = serialisasi(hasil)
+                        hasil += "\r\n\r\n"
+                        connection.sendall(hasil.encode())
+                        selesai = False
+                        data_received = ""  # string
+                        break
+                else:
+                    logging.warning(f"no more data from {client_address}")
+                    break
+        except ssl.SSLError as error_ssl:
+            logging.warning(f"SSL error: {str(error_ssl)}")
+
+if __name__=='__main__':
+    try:
+        run_server(('0.0.0.0', 12000))
+    except KeyboardInterrupt:
+        logging.warning("Control-C: Program berhenti")
+        exit(0)
+    finally:
+        logging.warning("selesai")
